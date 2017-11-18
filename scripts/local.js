cdac701f449f885d3218fd004b42c5441dc9dcd4
Dario <dario.dgm@gmail.com>
2017-11-18 15:38:32 -0300

    version final

--- scripts/local.js
+++ scripts/local.js
@@ -1,39 +1,42 @@
-AOS.init();
-$(window).on("scroll", function(){
-	if ($("body").scrollTop() > ($("header").height() - 100) ){
-		$(".contact, .wrapMenu").addClass("isBlack");
-	}else{
-		$(".contact, .wrapMenu").removeClass("isBlack");
-	}
+$(document).ready(() => {
+	AOS.init();
+	$(window).on("scroll", function() {
+		if ($("html").scrollTop() > $("header").height() - 100) {
+			$(".contact, .wrapMenu").addClass("isBlack");
+		} else {
+			$(".contact, .wrapMenu").removeClass("isBlack");
+		}
+	});
+	$(".menuToggle, .menu").on("click", function() {
+		$(".menu, .menuToggle").toggleClass("isActive");
+	});
+	$(".contact").on("mouseenter", () => {
+		$(".contactIcon").removeClass("fa fa-envelope-o");
+		$(".contactIcon").addClass("fa fa-envelope-open-o");
+	});
+	$(".contact").on("mouseleave", () => {
+		$(".contactIcon").removeClass("fa fa-envelope-open-o");
+		$(".contactIcon").addClass("fa fa-envelope-o");
+	});
+	$("#menuTratamientos, .more").on("click", function() {
+		$("html").animate({ scrollTop: $("#tratamientos").offset().top }, 900);
+	});
+	$("#menuMaquillaje").on("click", function() {
+		$("html").animate({ scrollTop: $("#maquillaje").offset().top }, 900);
+	});
+	$("#menuProductos").on("click", function() {
+		$("html").animate({ scrollTop: $("#productos").offset().top }, 900);
+	});
+	$("#menuAcerca").on("click", function() {
+		$("html").animate({ scrollTop: $("#acerca").offset().top }, 900);
+	});
+	$("#menuContacto, .contact").on("click", function() {
+		$("html").animate({ scrollTop: $("#contacto").offset().top }, 900);
+	});
+	$(".send").on("click", function() {
+		$(".pop").addClass("isActive");
+	});
+	$(".toggle").on("click", function() {
+		$(".pop").removeClass("isActive");
+	});
 });
-$(".menuToggle, .menu").on("click", function(){
-	$(".menu, .menuToggle").toggleClass("isActive");
-})
-$(".contact").on("click", function(){
-	$('body').animate({scrollTop: $("#contacto").offset().top}, 900);
-})
-
-$(".more").on("click", function(){
-	$('body').animate({scrollTop: $("#tratamientos").offset().top}, 900);
-})
-$("#menuTratamientos").on("click", function(){
-	$('body').animate({scrollTop: $("#tratamientos").offset().top}, 900);
-})
-$("#menuMaquillaje").on("click", function(){
-	$('body').animate({scrollTop: $("#maquillaje").offset().top}, 900);
-})
-$("#menuProductos").on("click", function(){
-	$('body').animate({scrollTop: $("#productos").offset().top}, 900);
-})
-$("#menuAcerca").on("click", function(){
-	$('body').animate({scrollTop: $("#acerca").offset().top}, 900);
-})
-$("#menuContacto").on("click", function(){
-	$('body').animate({scrollTop: $("#contacto").offset().top}, 900);
-})
-$(".send").on("click", function(){
-	$(".pop").addClass("isActive");
-})
-$(".toggle").on("click", function(){
-	$(".pop").removeClass("isActive");
-})
